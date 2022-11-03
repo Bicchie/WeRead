a15b8b84e3a30f19b5eaca8f10850f945482583b
Bicchie <i.bicchierini@studenti.unipi.it>
2022-11-03 17:45:50 +0100

    Added welcome page

---
+++ WeRead/src/main/java/it/unipi/dii/lsdb/weread/controller/WelcomePageController.java
@@ -0,0 +1,20 @@
+package it.unipi.dii.lsdb.weread.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import it.unipi.dii.lsdb.weread.persistence.Neo4jDriver;
+import it.unipi.dii.lsdb.weread.model.User;
+import it.unipi.dii.lsdb.weread.utils.Utils;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Controller for the Welcome page
+ */
+public class WelcomePageController {
+
+
+}
